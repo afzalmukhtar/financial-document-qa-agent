37b22db6923370f43b9d0f5656776356d80b60ea
Afzal Mukhtar Personal <afzalmukhtar985@gmail.com>
2026-02-11 21:57:42 +0530

    remove main.py and add contextual chunker skeleton for Adobe 10-K documents

---
+++ src/chunker/contextual_chunker.py
@@ -0,0 +1,9 @@
+class ContextualChunker:
+    """Custom chunker for Adobe 10-K financial documents."""
+
+    def __init__(self):
+        pass
+
+    def chunk(self, documents):
+        """Chunk documents into contextual pieces."""
+        raise NotImplementedError
